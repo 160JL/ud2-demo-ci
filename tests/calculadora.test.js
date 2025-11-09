8b0bfee7aa804b3bbdcce55fe1125e2faba25424
jeugfer600 <jeugfer600@g.educaand.es>
2025-11-09 20:46:38 +0100

    initial structure

---
+++ tests/calculadora.test.js
@@ -0,0 +1,19 @@
+// Importamos solo lo que necesitamos testear
+// ¡Actualizado para incluir 'restar'!
+const { sumar, restar } = require('../src/calculadora');
+
+// 'describe' agrupa tests relacionados. Es bueno para organizar.
+describe('Pruebas para la Calculadora', () => {
+
+  // 'it' (o 'test') define un caso de prueba individual.
+  // La descripción debe ser clara: "debería..."
+  it('debería sumar dos números correctamente', () => {
+    
+    // 'expect()' es la función de aserción de Jest.
+    // .toBe() es un "comparador" (matcher) que usa === (igualdad estricta).
+    expect(sumar(2, 2)).toBe(4);
+    expect(sumar(10, -5)).toBe(5);
+    expect(sumar(0, 0)).toBe(0);
+  });
+
+});
